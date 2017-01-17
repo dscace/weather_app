4c010e7f56ac2295e201edf2de64dc00d201a662
dscace <derek.scace@gmail.com>
2017-01-17 14:31:58 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  config.serve_static_assets = true
-  config.assets.compile = true
+  
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -84,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
