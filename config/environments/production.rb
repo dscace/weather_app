aa8ff0b02da42c0288988d6a1519fe3e7a4a30b3
dscace <derek.scace@gmail.com>
2017-01-17 14:13:06 -0700

    Back to normal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
