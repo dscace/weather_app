e2769d1e6ac6bac91caddf14310da2b9876bf365
dscace <derek.scace@gmail.com>
2017-01-18 00:26:36 -0700

    S3 almost ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = Proc.new do |source, request|
-    scheme = request.ssl? ? "https" : "http"
-    "#{scheme}://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  end
+  config.action_controller.asset_host = "#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
